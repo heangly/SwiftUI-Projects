c0d2bf3ebd79cb884863c1346afac8510f1fb1ca
Heang Ly <heangdev@gmail.com>
2021-11-04 00:01:30 -0400

    dismiss keyboard

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Heang Ly on 11/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
